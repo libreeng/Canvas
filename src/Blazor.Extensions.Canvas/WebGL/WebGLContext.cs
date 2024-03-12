c0611db4013401381613a15dec07ced6f0868a73
shawn <shawn@frozen.ca>
2024-03-12 10:36:30 -0500

    Removed all the blank lines.

--- src/Blazor.Extensions.Canvas/WebGL/WebGLContext.cs
+++ src/Blazor.Extensions.Canvas/WebGL/WebGLContext.cs
@@ -137,213 +137,111 @@ public class WebGLContext : RenderingContext
 
     #region Methods
     public async Task ClearColorAsync(float red, float green, float blue, float alpha) => await this.BatchCallAsync(CLEAR_COLOR, isMethodCall: true, red, green, blue, alpha);
-
     public async Task ClearAsync(BufferBits mask) => await this.BatchCallAsync(CLEAR, isMethodCall: true, mask);
-
     public async Task<WebGLContextAttributes> GetContextAttributesAsync() => await this.CallMethodAsync<WebGLContextAttributes>(GET_CONTEXT_ATTRIBUTES);
-
     public async Task<bool> IsContextLostAsync() => await this.CallMethodAsync<bool>(IS_CONTEXT_LOST);
-
     public async Task ScissorAsync(int x, int y, int width, int height) => await this.BatchCallAsync(SCISSOR, isMethodCall: true, x, y, width, height);
-
     public async Task ViewportAsync(int x, int y, int width, int height) => await this.BatchCallAsync(VIEWPORT, isMethodCall: true, x, y, width, height);
-
     public async Task ActiveTextureAsync(Texture texture) => await this.BatchCallAsync(ACTIVE_TEXTURE, isMethodCall: true, texture);
-
     public async Task BlendColorAsync(float red, float green, float blue, float alpha) => await this.BatchCallAsync(BLEND_COLOR, isMethodCall: true, red, green, blue, alpha);
-
     public async Task BlendEquationAsync(BlendingEquation equation) => await this.BatchCallAsync(BLEND_EQUATION, isMethodCall: true, equation);
-
     public async Task BlendEquationSeparateAsync(BlendingEquation modeRGB, BlendingEquation modeAlpha) => await this.BatchCallAsync(BLEND_EQUATION_SEPARATE, isMethodCall: true, modeRGB, modeAlpha);
-
     public async Task BlendFuncAsync(BlendingMode sfactor, BlendingMode dfactor) => await this.BatchCallAsync(BLEND_FUNC, isMethodCall: true, sfactor, dfactor);
-
     public async Task BlendFuncSeparateAsync(BlendingMode srcRGB, BlendingMode dstRGB, BlendingMode srcAlpha, BlendingMode dstAlpha) => await this.BatchCallAsync(BLEND_FUNC_SEPARATE, isMethodCall: true, srcRGB, dstRGB, srcAlpha, dstAlpha);
-
     public async Task ClearDepthAsync(float depth) => await this.BatchCallAsync(CLEAR_DEPTH, isMethodCall: true, depth);
-
     public async Task ClearStencilAsync(int stencil) => await this.BatchCallAsync(CLEAR_STENCIL, isMethodCall: true, stencil);
-
     public async Task ColorMaskAsync(bool red, bool green, bool blue, bool alpha) => await this.BatchCallAsync(COLOR_MASK, isMethodCall: true, red, green, blue, alpha);
-
     public async Task CullFaceAsync(Face mode) => await this.BatchCallAsync(CULL_FACE, isMethodCall: true, mode);
-
     public async Task DepthFuncAsync(CompareFunction func) => await this.BatchCallAsync(DEPTH_FUNC, isMethodCall: true, func);
-
     public async Task DepthMaskAsync(bool flag) => await this.BatchCallAsync(DEPTH_MASK, isMethodCall: true, flag);
-
     public async Task DepthRangeAsync(float zNear, float zFar) => await this.BatchCallAsync(DEPTH_RANGE, isMethodCall: true, zNear, zFar);
-
     public async Task DisableAsync(EnableCap cap) => await this.BatchCallAsync(DISABLE, isMethodCall: true, cap);
-
     public async Task EnableAsync(EnableCap cap) => await this.BatchCallAsync(ENABLE, isMethodCall: true, cap);
-
     public async Task FrontFaceAsync(FrontFaceDirection mode) => await this.BatchCallAsync(FRONT_FACE, isMethodCall: true, mode);
-
     public async Task<T> GetParameterAsync<T>(Parameter parameter) => await this.CallMethodAsync<T>(GET_PARAMETER, parameter);
-
     public async Task<Error> GetErrorAsync() => await this.CallMethodAsync<Error>(GET_ERROR);
-
     public async Task HintAsync(HintTarget target, HintMode mode) => await this.BatchCallAsync(HINT, isMethodCall: true, target, mode);
-
     public async Task<bool> IsEnabledAsync(EnableCap cap) => await this.CallMethodAsync<bool>(IS_ENABLED, cap);
-
     public async Task LineWidthAsync(float width) => await this.CallMethodAsync<object>(LINE_WIDTH, width);
-
     public async Task<bool> PixelStoreIAsync(PixelStorageMode pname, int param) => await this.CallMethodAsync<bool>(PIXEL_STORE_I, pname, param);
-
     public async Task PolygonOffsetAsync(float factor, float units) => await this.BatchCallAsync(POLYGON_OFFSET, isMethodCall: true, factor, units);
-
     public async Task SampleCoverageAsync(float value, bool invert) => await this.BatchCallAsync(SAMPLE_COVERAGE, isMethodCall: true, value, invert);
-
     public async Task StencilFuncAsync(CompareFunction func, int reference, uint mask) => await this.BatchCallAsync(STENCIL_FUNC, isMethodCall: true, func, reference, mask);
-
     public async Task StencilFuncSeparateAsync(Face face, CompareFunction func, int reference, uint mask) => await this.BatchCallAsync(STENCIL_FUNC_SEPARATE, isMethodCall: true, face, func, reference, mask);
-
     public async Task StencilMaskAsync(uint mask) => await this.BatchCallAsync(STENCIL_MASK, isMethodCall: true, mask);
-
     public async Task StencilMaskSeparateAsync(Face face, uint mask) => await this.BatchCallAsync(STENCIL_MASK_SEPARATE, isMethodCall: true, face, mask);
-
     public async Task StencilOpAsync(StencilFunction fail, StencilFunction zfail, StencilFunction zpass) => await this.BatchCallAsync(STENCIL_OP, isMethodCall: true, fail, zfail, zpass);
-
     public async Task StencilOpSeparateAsync(Face face, StencilFunction fail, StencilFunction zfail, StencilFunction zpass) => await this.BatchCallAsync(STENCIL_OP_SEPARATE, isMethodCall: true, face, fail, zfail, zpass);
-
     public async Task BindBufferAsync(BufferType target, WebGLBuffer buffer) => await this.BatchCallAsync(BIND_BUFFER, isMethodCall: true, target, buffer);
-
     public async Task BufferDataAsync(BufferType target, int size, BufferUsageHint usage) => await this.BatchCallAsync(BUFFER_DATA, isMethodCall: true, target, size, usage);
-
     public async Task BufferDataAsync<T>(BufferType target, T[] data, BufferUsageHint usage) => await this.BatchCallAsync(BUFFER_DATA, isMethodCall: true, target, this.ConvertToByteArray(data), usage);
-
     public async Task BufferSubDataAsync<T>(BufferType target, uint offset, T[] data) => await this.BatchCallAsync(BUFFER_SUB_DATA, isMethodCall: true, target, offset, this.ConvertToByteArray(data));
-
     public async Task<WebGLBuffer> CreateBufferAsync() => await this.CallMethodAsync<WebGLBuffer>(CREATE_BUFFER);
-
     public async Task DeleteBufferAsync(WebGLBuffer buffer) => await this.BatchCallAsync(DELETE_BUFFER, isMethodCall: true, buffer);
-
     public async Task<T> GetBufferParameterAsync<T>(BufferType target, BufferParameter pname) => await this.CallMethodAsync<T>(GET_BUFFER_PARAMETER, target, pname);
-
     public async Task<bool> IsBufferAsync(WebGLBuffer buffer) => await this.CallMethodAsync<bool>(IS_BUFFER, buffer);
-
     public async Task BindFramebufferAsync(FramebufferType target, WebGLFramebuffer framebuffer) => await this.BatchCallAsync(BIND_FRAMEBUFFER, isMethodCall: true, target, framebuffer);
-
     public async Task<FramebufferStatus> CheckFramebufferStatusAsync(FramebufferType target) => await this.CallMethodAsync<FramebufferStatus>(CHECK_FRAMEBUFFER_STATUS, target);
-
     public async Task<WebGLFramebuffer> CreateFramebufferAsync() => await this.CallMethodAsync<WebGLFramebuffer>(CREATE_FRAMEBUFFER);
-
     public async Task DeleteFramebufferAsync(WebGLFramebuffer buffer) => await this.BatchCallAsync(DELETE_FRAMEBUFFER, isMethodCall: true, buffer);
-
     public async Task FramebufferRenderbufferAsync(FramebufferType target, FramebufferAttachment attachment, RenderbufferType renderbuffertarget, WebGLRenderbuffer renderbuffer) => await this.BatchCallAsync(FRAMEBUFFER_RENDERBUFFER, isMethodCall: true, target, attachment, renderbuffertarget, renderbuffer);
-
     public async Task FramebufferTexture2DAsync(FramebufferType target, FramebufferAttachment attachment, Texture2DType textarget, WebGLTexture texture, int level) => await this.BatchCallAsync(FRAMEBUFFER_TEXTURE_2D, isMethodCall: true, target, attachment, textarget, texture, level);
-
     public async Task<T> GetFramebufferAttachmentParameterAsync<T>(FramebufferType target, FramebufferAttachment attachment, FramebufferAttachmentParameter pname) => await this.CallMethodAsync<T>(GET_FRAMEBUFFER_ATTACHMENT_PARAMETER, target, attachment, pname);
-
     public async Task<bool> IsFramebufferAsync(WebGLFramebuffer framebuffer) => await this.CallMethodAsync<bool>(IS_FRAMEBUFFER, framebuffer);
-
     public async Task ReadPixelsAsync(int x, int y, int width, int height, PixelFormat format, PixelType type, byte[] pixels) => await this.BatchCallAsync(READ_PIXELS, isMethodCall: true, x, y, width, height, format, type, pixels); //pixels should be an ArrayBufferView which the data gets read into
-
     public async Task BindRenderbufferAsync(RenderbufferType target, WebGLRenderbuffer renderbuffer) => await this.BatchCallAsync(BIND_RENDERBUFFER, isMethodCall: true, target, renderbuffer);
-
     public async Task<WebGLRenderbuffer> CreateRenderbufferAsync() => await this.CallMethodAsync<WebGLRenderbuffer>(CREATE_RENDERBUFFER);
-
     public async Task DeleteRenderbufferAsync(WebGLRenderbuffer buffer) => await this.BatchCallAsync(DELETE_RENDERBUFFER, isMethodCall: true, buffer);
-
     public async Task<T> GetRenderbufferParameterAsync<T>(RenderbufferType target, RenderbufferParameter pname) => await this.CallMethodAsync<T>(GET_RENDERBUFFER_PARAMETER, target, pname);
-
     public async Task<bool> IsRenderbufferAsync(WebGLRenderbuffer renderbuffer) => await this.CallMethodAsync<bool>(IS_RENDERBUFFER, renderbuffer);
-
     public async Task RenderbufferStorageAsync(RenderbufferType type, RenderbufferFormat internalFormat, int width, int height) => await this.BatchCallAsync(RENDERBUFFER_STORAGE, isMethodCall: true, type, internalFormat, width, height);
-
     public async Task BindTextureAsync(TextureType type, WebGLTexture texture) => await this.BatchCallAsync(BIND_TEXTURE, isMethodCall: true, type, texture);
-
     public async Task CopyTexImage2DAsync(Texture2DType target, int level, PixelFormat format, int x, int y, int width, int height, int border) => await this.BatchCallAsync(COPY_TEX_IMAGE_2D, isMethodCall: true, target, level, format, x, y, width, height, border);
-
     public async Task CopyTexSubImage2DAsync(Texture2DType target, int level, int xoffset, int yoffset, int x, int y, int width, int height) => await this.BatchCallAsync(COPY_TEX_SUB_IMAGE_2D, isMethodCall: true, target, level, xoffset, yoffset, x, y, width, height);
-
     public async Task<WebGLTexture> CreateTextureAsync() => await this.CallMethodAsync<WebGLTexture>(CREATE_TEXTURE);
-
     public async Task DeleteTextureAsync(WebGLTexture texture) => await this.BatchCallAsync(DELETE_TEXTURE, isMethodCall: true, texture);
-
     public async Task GenerateMipmapAsync(TextureType target) => await this.BatchCallAsync(GENERATE_MIPMAP, isMethodCall: true, target);
-
     public async Task<T> GetTexParameterAsync<T>(TextureType target, TextureParameter pname) => await this.CallMethodAsync<T>(GET_TEX_PARAMETER, target, pname);
-
     public async Task<bool> IsTextureAsync(WebGLTexture texture) => await this.CallMethodAsync<bool>(IS_TEXTURE, texture);
-
     public async Task TexImage2DAsync<T>(Texture2DType target, int level, PixelFormat internalFormat, int width, int height, PixelFormat format, PixelType type, T[] pixels)
         where T : struct
         => await this.BatchCallAsync(TEX_IMAGE_2D, isMethodCall: true, target, level, internalFormat, width, height, format, type, pixels);
-
     public async Task TexSubImage2DAsync<T>(Texture2DType target, int level, int xoffset, int yoffset, int width, int height, PixelFormat format, PixelType type, T[] pixels)
         where T : struct
         => await this.BatchCallAsync(TEX_SUB_IMAGE_2D, isMethodCall: true, target, level, xoffset, yoffset, width, height, format, type, pixels);
-
     public async Task TexParameterAsync(TextureType target, TextureParameter pname, float param) => await this.BatchCallAsync(TEX_PARAMETER_F, isMethodCall: true, target, pname, param);
-
     public async Task TexParameterAsync(TextureType target, TextureParameter pname, int param) => await this.BatchCallAsync(TEX_PARAMETER_I, isMethodCall: true, target, pname, param);
-
     public async Task AttachShaderAsync(WebGLProgram program, WebGLShader shader) => await this.BatchCallAsync(ATTACH_SHADER, isMethodCall: true, program, shader);
-
     public async Task BindAttribLocationAsync(WebGLProgram program, uint index, string name) => await this.BatchCallAsync(BIND_ATTRIB_LOCATION, isMethodCall: true, program, index, name);
-
     public async Task CompileShaderAsync(WebGLShader shader) => await this.BatchCallAsync(COMPILE_SHADER, isMethodCall: true, shader);
-
     public async Task<WebGLProgram> CreateProgramAsync() => await this.CallMethodAsync<WebGLProgram>(CREATE_PROGRAM);
-
     public async Task<WebGLShader> CreateShaderAsync(ShaderType type) => await this.CallMethodAsync<WebGLShader>(CREATE_SHADER, type);
-
     public async Task DeleteProgramAsync(WebGLProgram program) => await this.BatchCallAsync(DELETE_PROGRAM, isMethodCall: true, program);
-
     public async Task DeleteShaderAsync(WebGLShader shader) => await this.BatchCallAsync(DELETE_SHADER, isMethodCall: true, shader);
-
     public async Task DetachShaderAsync(WebGLProgram program, WebGLShader shader) => await this.BatchCallAsync(DETACH_SHADER, isMethodCall: true, program, shader);
-
     public async Task<WebGLShader[]> GetAttachedShadersAsync(WebGLProgram program) => await this.CallMethodAsync<WebGLShader[]>(GET_ATTACHED_SHADERS, program);
-
     public async Task<T> GetProgramParameterAsync<T>(WebGLProgram program, ProgramParameter pname) => await this.CallMethodAsync<T>(GET_PROGRAM_PARAMETER, program, pname);
-
     public async Task<string> GetProgramInfoLogAsync(WebGLProgram program) => await this.CallMethodAsync<string>(GET_PROGRAM_INFO_LOG, program);
-
     public async Task<T> GetShaderParameterAsync<T>(WebGLShader shader, ShaderParameter pname) => await this.CallMethodAsync<T>(GET_SHADER_PARAMETER, shader, pname);
-
     public async Task<WebGLShaderPrecisionFormat> GetShaderPrecisionFormatAsync(ShaderType shaderType, ShaderPrecision precisionType) => await this.CallMethodAsync<WebGLShaderPrecisionFormat>(GET_SHADER_PRECISION_FORMAT, shaderType, precisionType);
-
     public async Task<string> GetShaderInfoLogAsync(WebGLShader shader) => await this.CallMethodAsync<string>(GET_SHADER_INFO_LOG, shader);
-
     public async Task<string> GetShaderSourceAsync(WebGLShader shader) => await this.CallMethodAsync<string>(GET_SHADER_SOURCE, shader);
-
     public async Task<bool> IsProgramAsync(WebGLProgram program) => await this.CallMethodAsync<bool>(IS_PROGRAM, program);
-
     public async Task<bool> IsShaderAsync(WebGLShader shader) => await this.CallMethodAsync<bool>(IS_SHADER, shader);
-
     public async Task LinkProgramAsync(WebGLProgram program) => await this.BatchCallAsync(LINK_PROGRAM, isMethodCall: true, program);
-
     public async Task ShaderSourceAsync(WebGLShader shader, string source) => await this.BatchCallAsync(SHADER_SOURCE, isMethodCall: true, shader, source);
-
     public async Task UseProgramAsync(WebGLProgram program) => await this.BatchCallAsync(USE_PROGRAM, isMethodCall: true, program);
-
     public async Task ValidateProgramAsync(WebGLProgram program) => await this.BatchCallAsync(VALIDATE_PROGRAM, isMethodCall: true, program);
-
     public async Task DisableVertexAttribArrayAsync(uint index) => await this.BatchCallAsync(DISABLE_VERTEX_ATTRIB_ARRAY, isMethodCall: true, index);
-
     public async Task EnableVertexAttribArrayAsync(uint index) => await this.BatchCallAsync(ENABLE_VERTEX_ATTRIB_ARRAY, isMethodCall: true, index);
-
     public async Task<WebGLActiveInfo> GetActiveAttribAsync(WebGLProgram program, uint index) => await this.CallMethodAsync<WebGLActiveInfo>(GET_ACTIVE_ATTRIB, program, index);
-
     public async Task<WebGLActiveInfo> GetActiveUniformAsync(WebGLProgram program, uint index) => await this.CallMethodAsync<WebGLActiveInfo>(GET_ACTIVE_UNIFORM, program, index);
-
     public async Task<int> GetAttribLocationAsync(WebGLProgram program, string name) => await this.CallMethodAsync<int>(GET_ATTRIB_LOCATION, program, name);
-
     public async Task<T> GetUniformAsync<T>(WebGLProgram program, WebGLUniformLocation location) => await this.CallMethodAsync<T>(GET_UNIFORM, program, location);
-
     public async Task<WebGLUniformLocation> GetUniformLocationAsync(WebGLProgram program, string name) => await this.CallMethodAsync<WebGLUniformLocation>(GET_UNIFORM_LOCATION, program, name);
-
     public async Task<T> GetVertexAttribAsync<T>(uint index, VertexAttribute pname) => await this.CallMethodAsync<T>(GET_VERTEX_ATTRIB, index, pname);
-
     public async Task<long> GetVertexAttribOffsetAsync(uint index, VertexAttributePointer pname) => await this.CallMethodAsync<long>(GET_VERTEX_ATTRIB_OFFSET, index, pname);
-
     public async Task VertexAttribPointerAsync(uint index, int size, DataType type, bool normalized, int stride, long offset) => await this.BatchCallAsync(VERTEX_ATTRIB_POINTER, isMethodCall: true, index, size, type, normalized, stride, offset);
-
     public async Task UniformAsync(WebGLUniformLocation location, params float[] value)
     {
         switch (value.Length)
@@ -364,7 +262,6 @@ public class WebGLContext : RenderingContext
                 throw new ArgumentOutOfRangeException(nameof(value), value.Length, "Value array is empty or too long");
         }
     }
-
     public async Task UniformAsync(WebGLUniformLocation location, params int[] value)
     {
         switch (value.Length)
@@ -385,7 +282,6 @@ public class WebGLContext : RenderingContext
                 throw new ArgumentOutOfRangeException(nameof(value), value.Length, "Value array is empty or too long");
         }
     }
-
     public async Task UniformMatrixAsync(WebGLUniformLocation location, bool transpose, float[] value)
     {
         switch (value.Length)
@@ -403,7 +299,6 @@ public class WebGLContext : RenderingContext
                 throw new ArgumentOutOfRangeException(nameof(value), value.Length, "Value array has incorrect size");
         }
     }
-
     public async Task VertexAttribAsync(uint index, params float[] value)
     {
         switch (value.Length)
@@ -424,15 +319,10 @@ public class WebGLContext : RenderingContext
                 throw new ArgumentOutOfRangeException(nameof(value), value.Length, "Value array is empty or too long");
         }
     }
-
     public async Task DrawArraysAsync(Primitive mode, int first, int count) => await this.BatchCallAsync(DRAW_ARRAYS, isMethodCall: true, mode, first, count);
-
     public async Task DrawElementsAsync(Primitive mode, int count, DataType type, long offset) => await this.BatchCallAsync(DRAW_ELEMENTS, isMethodCall: true, mode, count, type, offset);
-
     public async Task FinishAsync() => await this.BatchCallAsync(FINISH, isMethodCall: true);
-
     public async Task FlushAsync() => await this.BatchCallAsync(FLUSH, isMethodCall: true);
-
     private byte[] ConvertToByteArray<T>(T[] arr)
     {
         byte[] byteArr = new byte[arr.Length * Marshal.SizeOf<T>()];
